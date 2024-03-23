aeac99dbcb887b968dc6d94b3171ea590bd662d0
Ian Korovinsky <ian.korovinsky@gmail.com>
2024-03-23 11:32:37 -0400

    Updated most of the top file, comments for components remaining

--- 03_LogicalStep_Lab4_top.vhd
+++ 03_LogicalStep_Lab4_top.vhd
@@ -1,9 +1,10 @@
 -- Members: Ian Korovinsky and Steven Yang | Lab4_REPORT | LS206 | Group 17
-
+-- Import packages
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
 USE ieee.numeric_std.ALL;
 
+-- Declares the top file input and output bits/vectors
 ENTITY LogicalStep_Lab4_top IS
    PORT
 	(
@@ -12,22 +13,21 @@ ENTITY LogicalStep_Lab4_top IS
 	pb_n			: in	std_logic_vector(3 downto 0); -- The push-button inputs (ACTIVE LOW)
  	sw   			: in  	std_logic_vector(7 downto 0); -- The switch inputs
     leds			: out 	std_logic_vector(7 downto 0);	-- for displaying the the lab4 project details
-	-------------------------------------------------------------
-	-- you can add temporary output ports here if you need to debug your design 
-	-- or to add internal signals for your simulations
-	-------------------------------------------------------------
 	
-   seg7_data 	: out 	std_logic_vector(6 downto 0); -- 7-bit outputs to a 7-segment
+    seg7_data 	: out 	std_logic_vector(6 downto 0); -- 7-bit outputs to a 7-segment
 	seg7_char1  : out	std_logic;							-- seg7 digi selectors
 	seg7_char2  : out	std_logic;							-- seg7 digi selectors
 	
-	-- Outputs for simulation
+	-- Outputs for simulation, clock enable, blink signal, and traffic lights simulated signals for the waveform
 	--sim_sm_clken, sim_blink_sig, sim_ns_green, sim_ns_amber, sim_ns_red, sim_ew_green, sim_ew_amber, sim_ew_red	:	out std_logic
 	);
 END LogicalStep_Lab4_top;
 
+-- Defines the architecture of the overall circuit
 ARCHITECTURE SimpleCircuit OF LogicalStep_Lab4_top IS
-   component segment7_mux port (
+   
+	-- Defines the segment7_mux component
+	component segment7_mux port (
              clk        	: in  	std_logic := '0';
 			 DIN2 			: in  	std_logic_vector(6 downto 0);	--bits 6 to 0 represent segments G,F,E,D,C,B,A
 			 DIN1 			: in  	std_logic_vector(6 downto 0); --bits 6 to 0 represent segments G,F,E,D,C,B,A
@@ -37,6 +37,7 @@ ARCHITECTURE SimpleCircuit OF LogicalStep_Lab4_top IS
    );
    end component;
 
+   -- Defines the clock generator component
    component clock_generator port (
 			sim_mode			: in boolean;
 			reset				: in std_logic;
@@ -46,6 +47,7 @@ ARCHITECTURE SimpleCircuit OF LogicalStep_Lab4_top IS
   );
    end component;
 
+   -- Defines the pb filter component
     component pb_filters port (
 			clkin				: in std_logic;
 			rst_n				: in std_logic;
@@ -55,6 +57,7 @@ ARCHITECTURE SimpleCircuit OF LogicalStep_Lab4_top IS
  );
    end component;
 
+   -- Defines the pb inverter component
 	component pb_inverters port (
 			rst_n				: in  std_logic;
 			rst				    : out	std_logic;							 
@@ -63,6 +66,7 @@ ARCHITECTURE SimpleCircuit OF LogicalStep_Lab4_top IS
   );
    end component;
 	
+   -- Defines the synchronizer component
 	component synchronizer port(
 			clk					: in std_logic;
 			reset					: in std_logic;
@@ -70,6 +74,8 @@ ARCHITECTURE SimpleCircuit OF LogicalStep_Lab4_top IS
 			dout					: out std_logic
   );
    end component; 
+
+   -- Defines the holding register component
   component holding_register port (
 			clk					: in std_logic;
 			reset					: in std_logic;
@@ -79,6 +85,7 @@ ARCHITECTURE SimpleCircuit OF LogicalStep_Lab4_top IS
   );
   end component;
 
+  -- Defines the state machine component
 	component State_Machine port (
 		clk_input, reset, sm_clken, blink_sig, ns_request, ew_request			: IN std_logic;
 		ns_green, ns_amber, ns_red, ew_green, ew_amber, ew_red						: OUT std_logic;
@@ -89,46 +96,61 @@ ARCHITECTURE SimpleCircuit OF LogicalStep_Lab4_top IS
 	);
 	end component;
 ----------------------------------------------------------------------------------------------------
-	CONSTANT	sim_mode								: boolean := TRUE;  -- set to FALSE for LogicalStep board downloads																						-- set to TRUE for SIMULATIONS
-	SIGNAL rst, rst_n_filtered, synch_rst			    : std_logic;
-	SIGNAL sm_clken, blink_sig							: std_logic; 
-	SIGNAL pb_n_filtered, pb							: std_logic_vector(3 downto 0); 
-	SIGNAL ew_req, ns_req					: std_logic;
-	SIGNAL ns_green, ns_amber, ns_red, ew_green, ew_amber, ew_red : std_logic;
-	SIGNAL ns_crossing, ew_crossing : std_logic;
-	SIGNAL NSLIGHTS, EWLIGHTS	: std_logic_vector(6 downto 0);
-	SIGNAL ns_clear, ew_clear : std_logic;
-	SIGNAL ew_out, ns_out : std_logic;
+	CONSTANT	sim_mode								: boolean := TRUE;  -- set to FALSE for LogicalStep board downloads
+	-- set to TRUE for SIMULATIONS
+	
+	-- All of the signals are defined below
+	SIGNAL rst, rst_n_filtered, synch_rst			    : std_logic; -- For holding reset values
+	SIGNAL sm_clken, blink_sig							: std_logic;  -- For holding the enable and blink signal
+	SIGNAL pb_n_filtered, pb							: std_logic_vector(3 downto 0);  -- For holding the button values
+	SIGNAL ew_req, ns_req					: std_logic; -- For holding the crossing requests
+	SIGNAL ns_green, ns_amber, ns_red, ew_green, ew_amber, ew_red : std_logic; -- For holding the traffic light values
+	SIGNAL ns_crossing, ew_crossing : std_logic; -- For holding the active crossing state values
+	SIGNAL NSLIGHTS, EWLIGHTS	: std_logic_vector(6 downto 0); -- For holding the overall constructed traffic digit value
+	SIGNAL ns_clear, ew_clear : std_logic; -- For holding the pedestrian request clear signals
+	SIGNAL ew_out, ns_out : std_logic; -- For holding the traffic light outputs
 	
 BEGIN
 ----------------------------------------------------------------------------------------------------
+-- Filters and inverts the buttons
 INST0: pb_filters		port map (clkin_50, rst_n, rst_n_filtered, pb_n, pb_n_filtered);
 INST1: pb_inverters		port map (rst_n_filtered, rst, pb_n_filtered, pb);
+-- Used to generate the waveform in part B
 --INST2: synchronizer     port map (clkin_50,synch_rst, rst, synch_rst);	-- the synchronizer is also reset by synch_rst.
-INST3: clock_generator 	port map (sim_mode, synch_rst, clkin_50, sm_clken, blink_sig);
 --leds(0) <= blink_sig;
 --leds(2) <= sm_clken;
 
+-- Used to generate the clock signal
+INST3: clock_generator 	port map (sim_mode, synch_rst, clkin_50, sm_clken, blink_sig);
+
+-- Used to activate the synchronizer which generates the synchronous reset signal
 INSTMID: synchronizer port map (clkin_50, synch_rst, rst, synch_rst);
 
+-- Synchronizer and holding register for the EW traffic light
 INSTEW: synchronizer port map (clkin_50, synch_rst, pb(1), ew_req);
 INSTEWHOLDREG: holding_register port map (clkin_50, synch_rst, ew_clear, ew_req, ew_out);
 leds(3) <= ew_out;
 
+-- Synchronizer and holding register for the NS traffic light
 INSTNS: synchronizer port map (clkin_50, synch_rst, pb(0), ns_req);
 INSTNSHOLDREG : holding_register port map (clkin_50, synch_rst, ns_clear, ns_req, ns_out);
 leds(1) <= ns_out;
 
+-- Generates an instance of the state machine which transitions between states and controls most of the traffic light
 INSTSTATEMACHINE : State_Machine port map (clkin_50, synch_rst, sm_clken, blink_sig, ns_out, ew_out, ns_green, ns_amber, ns_red, ew_green, ew_amber, ew_red, ns_crossing, ew_crossing, leds(7 downto 4), ns_clear, ew_clear);
 
+-- Displays the NS and EW crossing state values on the leds
 leds(0) <= ns_crossing;
 leds(2) <= ew_crossing;
 
+-- Holds the concatenated traffic light digit values
 NSLIGHTS(6 downto 0) <= ns_amber & "00" & ns_green & "00" & ns_red;
 EWLIGHTS(6 downto 0) <= ew_amber & "00" & ew_green & "00" & ew_red; 
 
+-- Uses the segment7_mux to display the traffic light digit values on the FPGA (or waveform if in SIM_MODE)
 INSTLIGHTS : segment7_mux port map (clkin_50, NSLIGHTS, EWLIGHTS, seg7_data, seg7_char2, seg7_char1);
 
+-- All of the simulated values for the final waveform
 --sim_sm_clken <= sm_clken;
 --sim_blink_sig <= blink_sig;
 --sim_ns_green <= ns_green;
